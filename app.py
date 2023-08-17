1c05ceb9e722f3d91be9b1ea32ce7238cbfac513
Tim Peterson <timothy.peterson.2014@gmail.com>
2023-08-17 17:46:05 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+import requests
+import simplejson
+import json
+from requests.auth import HTTPBasicAuth
+from collections import OrderedDict
+import settings
+from datetime import date
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/checkout')
+def checkout():
+    checkinData = {}
+    checkinsuri = "https://api.planningcenteronline.com/check-ins/v2/check_ins?where[created_at]=" + date.today().strftime("%Y-%m-%d") + "&order=checked_out_at&include=person"
+    try:
+        uResponse = requests.get(checkinsuri, auth = HTTPBasicAuth(settings.APIUser, settings.APIPass), headers={'X-PCO-API-Version': '2023-04-05'})
+    except requests.ConnectionError:
+        return "Connection Error"
+
+    responseData = uResponse.json()['data']
+
+    for i in responseData:
+        if i['attributes']['checked_out_at'] is not None:
+            checkinData[i['id']] = {"checked_out_at": i['attributes']['checked_out_at'], "first_name": i['attributes']['first_name'], "last_name": i['attributes']['last_name']}
+
+    return render_template('checkout.html', students=list(checkinData.values())[settings.listLength:])
---
+++ settings.py
@@ -0,0 +1,5 @@
+#Set variables
+APIUser = '3436075e85a5687d63eeca759159e36aee5ff2235dbb7340fb1af8bdc3e7177f'
+APIPass = 'eeaba70ad68a80924b0176b6decf15e7e9e641fd73051f61bbf5efee80edb1db'
+
+listLength = -10 #- mean last of list.  -10 would be last 10 checkouts.
