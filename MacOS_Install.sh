cc237dfc57656bd2c09e764e8ad220d5295c6a53
Tim <timothy.peterson.2014@gmail.com>
2023-08-20 20:17:45 -0400

    Creation of installation file

---
+++ MacOS_Install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#This script is used to install the Planning Center Checkout Page app onto a Mac.
+
+#Create the directory for the app to live
+mkdir /Library/WebServer/PlanningCenterCheckoutPage
+cp -a WebFiles/. /Library/WebServer/PlanningCenterCheckoutPage
+
+#Setup launcing app on startup
+cp ./local.planningCenterCheckout.plist /Library/LaunchAgents/local.planningCenterCheckout.plist
+
+#Move to the application directory
+cd /Library/WebServer/PlanningCenterCheckoutPage
+
+#Create the virtual python environment
+/usr/bin/python3 -m venv PCCPVENV
+pip3 install Flask
+pip3 install requests
+pip3 install simplejson
+
+shutdown -r now
